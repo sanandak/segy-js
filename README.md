8acdec13ddad3375c38774faeb9017f3ef3e374e
Sridhar Anandakrishnan <sxa17@psu.edu>
2017-03-16 09:56:43 -0400

    Private functions
    - added getBuf and bufToJSON as private functions
    - added readSegy as a separate fn from readSU
    - they now return {dt:dt, ns:ns, traces: [{...}, {...}]}
    rather than just traces
    - added MIT license
    - updated README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # segy-js
-Javascript SEG-Y reader
+=========
+
+A library to read SU (and eventually SEG-Y) files and return a JSON formatted array of traces.
+
+## Installation
+
+  npm install https://github.com/sanandak/segy-js
+
+## Usage
+
+  var readSU = require('segy-js').readSU
+  var traces = readSU('filename')
+
+## Tests
+
+  TBD
+
+## Contributing
+
+  TBD
+
+## Release History
+
+  * v0.1.0 Initial Release
--- index.js
+++ index.js
@@ -1,56 +1,79 @@
 var fs = require('fs');
 
-module.exports = {
-    readSegy: function(fname) {
-	var buf = fs.readFileSync(fname);
-	var ns = buf.readUInt16BE(114);
-	var dt = buf.readUInt16BE(116);
+var _getBuf =  function(fname) {
+    return fs.readFileSync(fname);
+}
+var _bufToJSON = function(buf) {
 
-	var trclen = 240 + ns * 4;
-	var ntrc = buf.length / trclen;
+  var ns = buf.readUInt16BE(114);
+  var dt = buf.readUInt16BE(116) / (1000. * 1000.);
 
-	if (ntrc !== Math.floor(ntrc))
-	    return({});
-	var traces = [];
-	for(var i=0; i<ntrc; i++) {
-	    var skip = i*trclen;
-	    var tracl = buf.readUInt32BE(0 + skip);
-	    var tracr = buf.readUInt32BE(4 + skip);
-	    var ffid  = buf.readUInt32BE(8 + skip); 
-	    var tracf = buf.readUInt32BE(12 + skip);
-	    var ep = buf.readUInt32BE(16 + skip);
-	    var cdp  = buf.readInt32BE(20 + skip);
-	    var cdpt = buf.readInt32BE(24+ skip);
-	    var offset = buf.readInt32BE(36);
-	    var sx = buf.readUInt32BE(72+ skip);
-	    var sy = buf.readUInt32BE(76+ skip);
-	    var gx = buf.readUInt32BE(80+ skip);
-	    var gy = buf.readUInt32BE(84+ skip);
-	    var ns = buf.readUInt16BE(114+ skip);
-	    var dt = buf.readUInt16BE(116+ skip) / 1000.;
+  var trclen = 240 + ns * 4;
+  var ntrc = buf.length / trclen;
 
-	    var samps = [];
-	    for(var j=0; j<ns; j++) {
-		samps.push({t:dt*j, v:buf.readFloatBE(240+j*4 + skip)});
-	    }
-	    traces.push({
-		tracl:tracl,
-		tracr:tracr,
-		ffid:ffid,
-		tracf:tracf,
-		ep:ep,
-		cdp:cdp,
-		cdpt:cdpt,
-		offset:offset,
-		sx:sx,
-		sy:sy,
-		gx:gx,
-		gy:gy,
-		ns:ns,
-		dt:dt,
-		samps:samps
-	    })
-	}
-	return traces; 
+  if (ntrc !== Math.floor(ntrc)) {
+    return({});
+  }
+
+  var traces = [];
+  for(var i=0; i<ntrc; i++) {
+    var skip = i*trclen;
+    var tracl = buf.readUInt32BE(0 + skip);
+    var tracr = buf.readUInt32BE(4 + skip);
+    var ffid  = buf.readUInt32BE(8 + skip);
+    var tracf = buf.readUInt32BE(12 + skip);
+    var ep = buf.readUInt32BE(16 + skip);
+    var cdp  = buf.readInt32BE(20 + skip);
+    var cdpt = buf.readInt32BE(24+ skip);
+    var offset = buf.readInt32BE(36);
+    var sx = buf.readUInt32BE(72+ skip);
+    var sy = buf.readUInt32BE(76+ skip);
+    var gx = buf.readUInt32BE(80+ skip);
+    var gy = buf.readUInt32BE(84+ skip);
+    // redundant? check consistency?
+    var ns = buf.readUInt16BE(114+ skip);
+    var dt = buf.readUInt16BE(116+ skip) / (1000. * 1000.); // us -> seconds
+
+    var samps = [];
+    for(var j=0; j<ns; j++) {
+      samps.push({t:dt*j, v:buf.readFloatBE(240+j*4 + skip)});
     }
+    traces.push({
+      tracl:tracl,
+      tracr:tracr,
+      ffid:ffid,
+      tracf:tracf,
+      ep:ep,
+      cdp:cdp,
+      cdpt:cdpt,
+      offset:offset,
+      sx:sx,
+      sy:sy,
+      gx:gx,
+      gy:gy,
+      ns:ns,
+      dt:dt,
+      samps:samps
+    })
+  }
+  return {dt:dt, ns: ns, traces:traces};
+}
+
+module.exports = {
+  readSegy: function(fname) {
+    var buf = this._getBuf(fname);
+    var textHdr = buf.slice(0,3200);
+    var binHdr = buf.slice(3200,3600);
+    var dt = binHdr.readUInt16BE(216);
+    var ns = binHdr.readUInt16BE(220);
+
+    var segy = _bufToJSON(buf.slice(3600));
+    return segy;
+  },
+
+  readSU: function(fname) {
+    var buf = _getBuf(fname);
+    var su = _bufToJSON(buf);
+    return su;
+  }
 };
