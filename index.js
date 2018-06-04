41b92479429de694efd74c67c3c6d287637400b4
Sridhar Anandakrishnan <sak@serenity.geosc.psu.edu>
2018-06-04 09:31:12 -0400

    testing using Promises

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var fs = require('fs');
 
+var _getNSDT = function(fname) {
+  var buffer = new Buffer(240);
+  return new Promise(function(resolve, reject) {
+    var fd = fs.open(fname, 'r', 0o666, function(err, fd) {
+      if(err) return reject(err);
+      fs.read(fd, buffer, 0, 240, null, function(err, nbytes, buffer) {
+        if (err) return reject(err);
+        else {
+          var ns = buffer.readUInt16BE(114);
+          var dt = buffer.readUInt16BE(116) / (1000. * 1000.);
+          fs.close(fd);
+          resolve({ns:ns, dt:dt});
+        }
+      })
+    })
+  })
+};
+/*
+  var ns, dt;
+  var hdrLen = fs.readSync(suFD, buf, 0, 240, null);
+  var finfo = fs.statSync(fname);
+
+  ns = buf.readUInt16BE(114);
+  dt = buf.readUInt16BE(116) / (1000. * 1000.);
+
+  var ntrc = finfo.size / (240 + ns * 4);
+  console.log('r',ns, dt, ntrc);
+  return {ns:ns, dt:dt, ntrc:ntrc};
+}
+*/
+
 var _getBuf =  function(fname) {
     return fs.readFileSync(fname);
 }
@@ -69,6 +100,16 @@ module.exports = {
 
     var segy = _bufToJSON(buf.slice(3600));
     return segy;
+  },
+
+  getNSDT: async function(fname) {
+    try {
+      return nsdt = await _getNSDT(fname);
+//      console.log(nsdt);
+//      return(nsdt);
+    } catch(err) {
+      console.error('Error reading ${fname}');
+    }
   },
 
   readSU: function(fname) {
